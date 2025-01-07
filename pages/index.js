af41e12108b2db3579e51ba497b7287693525c46
Plesh4kov <plesh4kov@gmail.com>
2025-01-06 21:12:03 -0800

    Fixed scoreboard rendering and updated design

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   const TOKEN_MINT = "3EnxSbUszY4XPDfmWRJa3jkBUX4Dn8rdh4ZkNQCwpump";
 
   // Ваш RPC-эндпоинт (если токен на mainnet)
-  const SOLANA_RPC_ENDPOINT = "https://api.mainnet-beta.solana.com";
+  const SOLANA_RPC_ENDPOINT = "https://rpc.ankr.com/solana/3226dc3c471ff22f8355da4308ddd4a219b5ad67a62557acde28a539b94a550e";
   // Или, например, Ankr: "https://rpc.ankr.com/solana/..."
   // Или Devnet: "https://api.devnet.solana.com"
 
